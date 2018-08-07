793adc2338fdcf1cb6cf54632969b7551d84205d
Tremble666 <1435016360@qq.com>
2018-08-07 16:52:25 +0800

    [Update] 1 file

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Spring + Mybatis
 
 ## 运行
 运行指令 'bash build.sh'
+
+#### 说明几点问题
+1. 第四题的Context Start/stop的代码在config包下
+2. 第五，六题的代码在util包下，配置在applicationContext-mybatis.xml文件最下方
+3. applicationContext-mybatisbak.xml和dbbakproperties是我的测试文件，与项目最终结果无关
--- src/main/java/com/hand/Application.java
+++ src/main/java/com/hand/Application.java
@@ -18,13 +18,16 @@ public class Application {
 
 
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        System.out.println("Context Start!!!");
+        System.out.println("Context Start");
         FilmService filmService =context.getBean(FilmService.class);
         Film f = new Film();
         f.setTitle(title);
+        System.out.println("title:"+title);
         f.setDescription(description);
+        System.out.println("description:"+description);
         f.setRelease_year("2006");
         f.setLanguage_id(Integer.valueOf(languageId));
+        System.out.println("languageId:"+languageId);
         f.setOriginal_language_id(null);
         f.setRental_duration(4);
         f.setRental_rate(new BigDecimal("22"));
@@ -35,6 +38,6 @@ public class Application {
         f.setLast_update("2006-02-15 05:03:42");
         filmService.save(f);
         System.out.println(f);
-        System.out.println("All works have succeed!! ");
+        System.out.println("Context Stop");
     }
 }
